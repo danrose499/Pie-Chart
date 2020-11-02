81a25ed7dd8677b9756de511933003d87a4db8af
Daniel Rosenthal <57165307+danrose499@users.noreply.github.com>
2020-11-01 23:18:26 -0500

    Added methods/constructors to histogram class

--- HistogramAlphaBet.java
+++ HistogramAlphaBet.java
@@ -1,4 +1,58 @@
 package sample;
 
+import javafx.scene.canvas.GraphicsContext;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import static java.util.stream.Collectors.toMap;
+
 public class HistogramAlphaBet {
+    String filename;
+    int totalChars;
+    Map<Character, Integer> sortedFrequency;
+
+    HistogramAlphaBet(String filename) {
+        this.filename = filename;
+    }
+
+    public void getMap() throws IOException {
+        Path fileName = Path.of(filename);
+        String actual = Files.readString(fileName);
+        String s = actual.replaceAll("[^a-zA-Z]", "").toLowerCase();
+        totalChars = s.length();
+
+        Map<Character, Integer> frequency = new HashMap<>();
+
+        for(int i = 0; i < s.length(); i++) {
+            Character Ch = s.charAt(i);
+            incrementFrequency(frequency, Ch);
+        }
+
+        this.sortedFrequency = frequency
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .collect(toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2, LinkedHashMap::new));
+    }
+
+    public static <K> void incrementFrequency(Map<K, Integer> m, K Key) {
+        m.putIfAbsent(Key, 0);
+        m.put(Key, m.get(Key) + 1);
+    }
+
+    public void drawPieChart(GraphicsContext GC, int SlicesToPrint) throws IOException {
+        getMap();
+        int cWidth = (int) GC.getCanvas().getWidth();
+        int cHeight = (int) GC.getCanvas().getHeight();
+        double d = (4.5 * (Math.min(cWidth, cHeight) / 2.0) / 3.0)-50;
+        MyPoint arcPoint = new MyPoint(cWidth / 2 - (int) d / 2, cHeight / 2 - (int) d / 2);
+        PieChart charChart = new PieChart(arcPoint, d);
+        charChart.draw(GC, totalChars, SlicesToPrint, sortedFrequency);
+    }
 }
